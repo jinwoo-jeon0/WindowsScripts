082ef22fa5ad903df8b73cddc46f86f8d77210d9
Jinwoo Jeon <jinwoo.jeon0@gmail.com>
2024-01-03 19:00:25 +0900

    Extract no-encoder.ps1, dd.ps1, dts.ps1

---
+++ sound-blaster-command/no-encoder.ps1
@@ -0,0 +1,4 @@
+. "$PSScriptRoot\select-encoder.ps1"
+
+MoveMouse -x 279 -y 883
+Click
--- sound-blaster-command/select-encoder.ps1
+++ sound-blaster-command/select-encoder.ps1
@@ -6,14 +6,3 @@ $ErrorActionPreference = "Stop"
 
 MoveMouse -x 27 -y 1104
 Click
-
-MoveMouse -x 279 -y 883
-Click
-
-# Dolby Audio
-# MoveMouse -x 279 -y 948
-# Click
-
-# DTS
-# MoveMouse -x 279 -y 1031
-# Click
