b7e7b60cb4cd3734a010be64d3ec19f1f63c2ee9
Jinwoo Jeon <jinwoo.jeon0@gmail.com>
2024-01-03 18:50:37 +0900

    Extract mouse.ps1

--- sound-blaster-command/select-encoder.ps1
+++ sound-blaster-command/select-encoder.ps1
@@ -1,28 +1,8 @@
+$ErrorActionPreference = "Stop"
+
 . "$PSScriptRoot\activate.ps1"
 
-Add-Type -AssemblyName System.Windows.Forms
-
-Function MoveMouse {
-    Param ([int]$x, [int]$y)
-
-    Process {
-        [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
-    }
-}
-
-$Mouse=@'
-[DllImport("user32.dll",CharSet=CharSet.Auto, CallingConvention=CallingConvention.StdCall)]
-public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
-'@
-
-$SendMouseClick = Add-Type -memberDefinition $Mouse -name "Win32MouseEventNew" -namespace Win32Functions -passThru
-
-Function Click {
-    Process {
-        $SendMouseClick::mouse_event(0x00000002, 0, 0, 0, 0);
-        $SendMouseClick::mouse_event(0x00000004, 0, 0, 0, 0);
-    }
-}
+. "$PSScriptRoot\..\mouse.ps1"
 
 MoveMouse -x 27 -y 1104
 Click
